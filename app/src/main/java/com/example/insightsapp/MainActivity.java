81c7be04f463b863d97bdf3bfadf1300d9df6ec1
RohanSharma4792 <your rohansharma4792@gmail.com>
2022-07-08 18:39:03 +0530

    register button

--- app/src/main/java/com/example/insightsapp/MainActivity.java
+++ app/src/main/java/com/example/insightsapp/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.insightsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signup = (TextView) findViewById(R.id.signup);
+        signup.setOnClickListener(this);
     }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.signup:
+                startActivity(new Intent(this, signup.class));
+                break;
+        }
+
+    }
+
+
 }
