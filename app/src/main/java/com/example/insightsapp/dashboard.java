f88c1c0d44fbf69ba675c6b1c95ae2e277adfe46
RohanSharma4792 <your rohansharma4792@gmail.com>
2022-07-11 20:29:19 +0530

    firebase fetch remaining

--- app/src/main/java/com/example/insightsapp/dashboard.java
+++ app/src/main/java/com/example/insightsapp/dashboard.java
@@ -1,9 +1,11 @@
 package com.example.insightsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -12,8 +14,12 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.auth.User;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -23,9 +29,7 @@ public class dashboard extends AppCompatActivity {
     TextView name;
     TextView email;
     TextView mobile;
-    FirebaseAuth auth;
-    DatabaseReference databaseReference;
-    StorageReference storageReference;
+
 
 
     @Override
@@ -37,17 +41,6 @@ public class dashboard extends AppCompatActivity {
         name = findViewById(R.id.textView9);
         email = findViewById(R.id.textView10);
         mobile = findViewById(R.id.textView11);
-
-
-
-
-
-
-
-
-
-
-
 
 
         logout.setOnClickListener(new View.OnClickListener() {
@@ -62,5 +55,28 @@ public class dashboard extends AppCompatActivity {
                 finish();
             }
         });
-    }
-}
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Data");
+            reference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists()){
+                        String atr = snapshot.getValue().toString();
+                        String btr = snapshot.getValue().toString();
+                        String ctr = snapshot.getValue().toString();
+
+                        name.setText(atr);
+                        email.setText(btr);
+                        mobile.setText(ctr);
+
+
+
+
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            })
+        ;}}
